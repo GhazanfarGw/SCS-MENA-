209c81d0631cc14255a656810ce3717f9488e8ef
Muhammad Ghazanfar <116085344+GhazanfarGw@users.noreply.github.com>
2025-05-19 08:36:07 +0500

    Update About.jsx

--- src/Home/About.jsx
+++ src/Home/About.jsx
@@ -14,7 +14,7 @@ function About() {
                 <div className="pt-10 justify-center md:text-center text-left mx-auto">
                     <div className='md:mx-auto justify-center md:text-center text-left max-w-screen-md'>
                         <h1 data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='md:text-center text-left lg:text-4xl text-2xl md:pt-5 font-medium text-[#1A2B6B]'>
-                            Trusted experts in Crypto Compliance, Aml, sanctions and risk management
+                            Trusted experts in Crypto Compliance, AML, sanctions and risk management
                         </h1>
                         <p data-aos="zoom-in-up" data-aos-delay="300" data-aos-duration="400" className='md:text-center text-left pt-5 md:text-base leading-normal text-sm text-[#6D7380]'>
                             Strategic Compliance Solutions (SCS) MENA is a specialized compliance consultancy providing expert advisory services in Crypto Compliance, Sanctions, AML, Trade Finance and Export Controls Compliance, Investigations, Remediation Projects and Financial Crime compliance. With extensive experience across UAE, UK, and Global Financial Markets, we support Financial Institutions, VASPs, Fintech firms, and corporates in navigating complex regulatory landscapes.
